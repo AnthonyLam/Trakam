3e94381c447e3c4591f23674ea3d8afcaac515b9
Anthony Lam <thelamacmdr@gmail.com>
2018-03-04 04:49:53 +0000

    UPdate python shit

--- lib/TestPhoto.py
+++ lib/TestPhoto.py
@@ -1,49 +1,36 @@
 import cognitive_face as CF
 import sys
 import pprint
-from PIL import Image, ImageDraw, ImageFont
+import uuid
+import time
 
 pp = pprint.PrettyPrinter(indent=4)
-
-if len(sys.argv) < 2:
-    print("This is no good")
-    sys.exit()
-
 KEY = '90823e5520ad43b3b8e828ba6b1a7a3e'
 CF.Key.set(KEY)
 
-file = sys.argv[1]
+def detect(file):
+    print("file = {}\n".format(file))
+        
+    detectOut = CF.face.detect(file, attributes='age')
+    pp.pprint(detectOut)
+    output = []
+    
+    if(len(detectOut) > 0):
+        output = (list(map(lambda x: x['faceId'], detectOut)))
+        
+        identifyOut = CF.face.identify(output, 'people')
+        pp.pprint(identifyOut)
+        
+        for x in identifyOut:
+            for candidate in x['candidates']:
+                name = CF.person.get('people', candidate['personId'])['name']
+                output.append("{},{},{}".format(str(uuid.uuid4()), name, int(time.time() * 1000)))
+    return output
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("This is no good")
+        sys.exit()
 
-print("file = {}\n".format(file))
-
-detectOut = CF.face.detect(file, attributes='age')
-pp.pprint(detectOut)
-
-output = (list(map(lambda x: x['faceId'], detectOut)))
-
-identifyOut = CF.face.identify(output, 'people')
-pp.pprint(identifyOut)
-
-for x in identifyOut:
-    for candidate in x['candidates']:
-        print(CF.person.get('people', candidate['personId'])['name'])
-
-im = Image.open(file)
-draw = ImageDraw.Draw(im)
-fnt = ImageFont.truetype('Raleway-Regular.ttf', 40)
-
-for detection in detectOut:
-    rect = detection["faceRectangle"]
-    x0 = rect["left"]
-    y0 = rect["top"]
-    x1 = rect["width"] + x0
-    y1 = rect["height"] + y0
-    draw.rectangle([x0, y0, x1, y1])
-    io = CF.face.identify([detection["faceId"]], 'people')
-    for x in io:
-        for candidate in x['candidates']:
-            draw.text([x0, y0], CF.person.get('people', candidate['personId'])['name'], font=fnt, fill=(255, 0, 0))
-
-del draw
-
-im.save(file.split(sep=".")[0] + "ed.png", "PNG")
+    file = sys.argv[1]
+    detect(file)
--- lib/protocol.py
+++ lib/protocol.py
@@ -1,32 +1,40 @@
 import wiringpi
+import TestPhoto
+import time
 
 wiringpi.wiringPiSetup()
 serial = wiringpi.serialOpen("/dev/ttyAMA0", 9600)
 
-thing = True
-
-while(thing):
+while(True):
     wiringpi.serialPutchar(serial, 55)
+    
+    if(wiringpi.serialDataAvail(serial) > 0):
+        code = wiringpi.serialGetchar(serial)
+        print("Recieved: ", code)
+    
+        if(code == 11):
+            size = []
+            for i in range(4):
+                size.append(wiringpi.serialGetchar(serial))
+            sizeI = sum(map(lambda x: max(x[0]*256, 1)*x[1], enumerate(size[::-1])))
+            print("Payload size:", sizeI)
+    
+            imageBytes = []
+            for i in range(sizeI):
+               imageBytes.append(wiringpi.serialGetchar(serial))
+    
+            newFile = open("test.jpg", "wb")
+            newFile.write(bytes(imageBytes))
+            newFile.flush()
+            fileout = TestPhoto.detect("test.jpg")
+            with open("logs/foo.log", "a") as f:
+                for o in fileout:
+                    f.write(o + "\n")
 
-    code = wiringpi.serialGetchar(serial)
-    print("Recieved: ", code)
-
-    if(code == 11):
-        size = []
-        for i in range(4):
-            size.append(wiringpi.serialGetchar(serial))
-        sizeI = sum(map(lambda x: max(x[0]*256, 1)*x[1], enumerate(size[::-1])))
-        print("Payload size:", sizeI)
-
-        imageBytes = []
-        for i in range(sizeI):
-           imageBytes.append(wiringpi.serialGetchar(serial))
-
-        newFile = open("test.jpg", "wb")
-        newFile.write(bytes(imageBytes))
-
-    thing = False
-
+    else:
+        print("No response received.")
+    time.sleep(1)
+    
 wiringpi.serialFlush(serial)
 wiringpi.serialClose(serial)
 
