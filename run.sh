56087f9cc55343410d53c6c56a571992870aa494
Anthony Lam <thelamacmdr@gmail.com>
2018-03-04 15:10:10 -0800

    Fix run script

--- run.sh
+++ run.sh
@@ -5,13 +5,15 @@ mkdir logs
 mkdir img
 
 # Build and run go server
-printf "Running go server"
-go build ./server
-./Trakam &
+printf "Running go server\n"
+cd server
+go build
+cd ..
+./server/server &
 echo $!
 
 # Run python stuff
-printf "Running python program"
+printf "Running python program\n"
 source lib/venv/bin/activate
 python3 lib/protocol.py &
 echo $1
