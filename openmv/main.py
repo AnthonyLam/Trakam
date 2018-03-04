0a9834ab9190f12766cc9a183a1095ae03794453
Anthony Lam <thelamacmdr@gmail.com>
2018-03-04 13:41:27 -0800

    UART working

--- openmv/main.py
+++ openmv/main.py
@@ -14,7 +14,7 @@ sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_contrast(1)
 sensor.set_gainceiling(16)
 sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.HQVGA)   # Set frame size to QQVGA (160 x 120)
+sensor.set_framesize(sensor.HQVGA)   # Set frame size to HQVGA
 sensor.skip_frames(time = 2000)     # Wait for settings take effect.
 
 
@@ -23,14 +23,11 @@ sensor.skip_frames(time = 2000)     # Wait for settings take effect.
 #uart.init(9600, bits=8, parity=None, stop=1, timeout_char=1000)
 
 # Initialize UART communication
-# pi = UART(1)
-# pi.init(115200, bits=8, parity=0, stop=1, timeout_char=2000)
-
-pi = SPI(2)
-pi.init(2, SPI.SLAVE, polarity=0, phase=0, bits=8)
+pi = UART(1)
+pi.init(921600, bits=8, parity=0, stop=1, timeout_char=2000)
 
 front_face = image.HaarCascade("frontalface", stages=25)
-print(front_face)
+print("Front Face Cascade:", front_face)
 
 TOP_CHAN = 0
 BOT_CHAN = 1
@@ -58,12 +55,12 @@ while(True):
     if invalidated:
         c = pyb.millis()
 
-    if pi.recv(1, timeout=5) == 55:
+    if pi.any() and pi.readchar() == 55:
         print("Request Received")
         cimg = img.compress()
         valid = 11 if invalidated and blobs else 00
         print("Sending image. Detect: {}".format(valid))
-        pi.send(valid)
-        pi.send(pack('>i', cimg.size()))
-        pi.send(cimg)
+        pi.writechar(valid)
+        pi.write(pack('>i', cimg.size()))
+        pi.write(cimg)
     saved = list(blobs)
