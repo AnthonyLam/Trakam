555b3d4a0a19746810eff63a0c6846c095b67ecd
Anthony Lam <thelamacmdr@gmail.com>
2018-03-04 22:05:44 -0800

    Only send image on valid

--- openmv/main.py
+++ openmv/main.py
@@ -59,8 +59,10 @@ while(True):
         print("Request Received")
         cimg = img.compress()
         valid = 11 if invalidated and blobs else 00
-        print("Sending image. Detect: {}".format(valid))
-        pi.writechar(valid)
-        pi.write(pack('>i', cimg.size()))
-        pi.write(cimg)
+        if valid:
+            print("Sending image. Detect: {}".format(valid))
+
+            pi.writechar(valid)
+            pi.write(pack('>i', cimg.size()))
+            pi.write(cimg)
     saved = list(blobs)
