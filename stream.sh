0470297964e6d1cb755078ec35dbe41bcb7d183d
trakam@pi <pi@trakam.com>
2018-03-11 20:59:22 +0000

    Read from /var/img

--- stream.sh
+++ stream.sh
@@ -1,4 +1,4 @@
 MJPG_PATH=/home/pi/mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=$MJPG_PATH
 
-$MJPG_PATH/mjpg_streamer -i "input_http.so -p 4081" -o "output_http.so -l 0.0.0.0 -p 8090 -w ./www"
+$MJPG_PATH/mjpg_streamer -i "input_file.so -d 0.01 -f /var/img/" -o "output_http.so -l 0.0.0.0 -p 8090 -w ./www"
