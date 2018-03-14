19a38041285ca4142e5768793b385689d21351fe
Darshan Parajuli <parajulidarshan@gmail.com>
2018-03-13 19:04:10 -0700

    check user input for settings

--- android/Trakam/app/src/main/java/com/trakam/trakam/fragments/settings/SettingsFragment.kt
+++ android/Trakam/app/src/main/java/com/trakam/trakam/fragments/settings/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.preference.PreferenceFragment
 import android.preference.PreferenceManager.getDefaultSharedPreferences
 import com.trakam.trakam.R
 import com.trakam.trakam.util.PrefKeys
+import com.trakam.trakam.util.showToast
 
 class SettingsFragment : PreferenceFragment() {
 
@@ -23,34 +24,66 @@ class SettingsFragment : PreferenceFragment() {
         setupCategoryLiveFeed()
     }
 
+    private fun setupCategoryServer() {
+        val sharedPrefs = getDefaultSharedPreferences(activity)
+        val prefHost = findPreference(PrefKeys.Server.KEY_HOST)
+        prefHost.summary = sharedPrefs.getString(PrefKeys.Server.KEY_HOST,
+                PrefKeys.Server.Default.HOST)
+        prefHost.setOnPreferenceChangeListener Callback@{ preference, newValue ->
+            try {
+                val input = (newValue as String).trim()
+                if (input.contains("\n")) {
+                    activity!!.showToast("Invalid input")
+                    return@Callback false
+                }
+                preference.summary = input
+                true
+            } catch (e: Exception) {
+                activity!!.showToast("Invalid input")
+                false
+            }
+        }
+
+        val prefPort = findPreference(PrefKeys.Server.KEY_PORT)
+        prefPort.summary = sharedPrefs.getString(PrefKeys.Server.KEY_PORT,
+                PrefKeys.Server.Default.PORT)
+        prefPort.setOnPreferenceChangeListener Callback@{ preference, newValue ->
+            try {
+                val input = (newValue as String).trim()
+                if (input.contains("\n")) {
+                    activity!!.showToast("Invalid input")
+                    return@Callback false
+                }
+                Integer.parseInt(input)
+                preference.summary = input
+                true
+            } catch (e: Exception) {
+                activity!!.showToast("Invalid input")
+                false
+            }
+        }
+    }
+
     private fun setupCategoryLiveFeed() {
         val sharedPrefs = getDefaultSharedPreferences(activity)
         val prefPort = findPreference(PrefKeys.LiveFeed.KEY_PORT)
         prefPort.summary = sharedPrefs.getString(PrefKeys.LiveFeed.KEY_PORT,
                 PrefKeys.LiveFeed.Default.PORT)
 
-        prefPort.setOnPreferenceChangeListener { preference, newValue ->
-            preference.summary = newValue as String
-            true
-        }
-    }
-
-    private fun setupCategoryServer() {
-        val sharedPrefs = getDefaultSharedPreferences(activity)
-        val prefHost = findPreference(PrefKeys.Server.KEY_HOST)
-        prefHost.summary = sharedPrefs.getString(PrefKeys.Server.KEY_HOST,
-                PrefKeys.Server.Default.HOST)
-        prefHost.setOnPreferenceChangeListener { preference, newValue ->
-            preference.summary = newValue as String
-            true
-        }
-
-        val prefPort = findPreference(PrefKeys.Server.KEY_PORT)
-        prefPort.summary = sharedPrefs.getString(PrefKeys.Server.KEY_PORT,
-                PrefKeys.Server.Default.PORT)
-        prefPort.setOnPreferenceChangeListener { preference, newValue ->
-            preference.summary = newValue as String
-            true
+        prefPort.setOnPreferenceChangeListener Callback@{ preference, newValue ->
+            try {
+                val input = (newValue as String).trim()
+                if (input.contains("\n")) {
+                    activity!!.showToast("Invalid input")
+                    return@Callback false
+                }
+                Integer.parseInt(input)
+                preference.summary = input
+                true
+            } catch (e: Exception) {
+                activity!!.showToast("Invalid input")
+                false
+            }
         }
     }
 
